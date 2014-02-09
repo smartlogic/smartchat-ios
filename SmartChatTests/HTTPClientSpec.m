5fba324d3ffb7aa59f72fb7e00a10c529eeea55c
Tom von Schwerdtner <tom@smartlogic.io>
2014-02-09 09:52:29 -0500

    Reformat tests for readability.
    Also, add a missing 'it' block.

--- SmartChatTests/HTTPClientSpec.m
+++ SmartChatTests/HTTPClientSpec.m
@@ -15,14 +15,14 @@ describe(@"HTTPClient", ^{
                                                            encoding:NSUTF8StringEncoding
                                                               error:NULL];
         HTTPClient *client = [[HTTPClient alloc] initWithCredentials:credentials];
-        NSString *expected = @"KZ7D/5A0M0EUcNvULw3irkU8aTIdeOKE12aDEaXPxelishdlkIKd9U/D8tbCuPfX"
+        NSString *expected = @""
+        "KZ7D/5A0M0EUcNvULw3irkU8aTIdeOKE12aDEaXPxelishdlkIKd9U/D8tbCuPfX"
         "dZvXxhcV8JHFLKOF0k2OLG05sIO5bRN7RDhkTadTtuSxyuIK5RvZhHTE2VVjCWyP"
         "0rvpag/CckCbhlI76V79zU/ZQhpu1xklBtEhaoqWB4zTFa2LhWz4b8/wV0OJ3fW1"
         "soqquCN26FSu019zeghoquTXEbmVfXvQdyio9wTAFYx9zjDI/qTCkvqr9pK4xo5P"
         "72atlkmYSNNL72uoYzat1V2+FMcy34HzLRL6wNMAWZtXvb8rXuVMTOCUjb0VwxNI"
         "JoIZHP+3HCijqn6QNNl6PA==";
 
-        
         NSString *result = [client signedPath:@"http://example.org/example"];
         [[result should] equal:expected];
     });
--- SmartChatTests/NSStringKeySigningSpec.m
+++ SmartChatTests/NSStringKeySigningSpec.m
@@ -4,17 +4,25 @@
 SPEC_BEGIN(NSStringKeySigningSpec)
 
 describe(@"NSString+KeySigning", ^{
+    
+    it(@"signs a string with a provided private key", ^{
+        NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:@"private_key" ofType:@"pem"];
+        NSString *privateKeyString = [NSString stringWithContentsOfFile:path
+                                                               encoding:NSUTF8StringEncoding
+                                                                  error:NULL];
+        NSString *expected = @""
+        "fQjETWgIMqXEhNyJ52VdsZJIuXX+RHXO4wT5sFtMJBUMTpidksZ8NaQ3Fp1N"
+        "Yy2mJEee7Y835IOuFrGpkRroZWnO6s+jvTfWCmaK4DkRKVOB3DbS/o37+PA1"
+        "aGVHr7lKh0HHPk6tv7chxUbcdNKC+AWR5/m2DxehhAl23tdyDK2qPr0u2HLv"
+        "zgHNnR4TQafjGOKVAekLdTLu0CUfd8fdzpUEJK5TdUkd6rA5zoORjsLOZXY/"
+        "7cbanCzs2nNii9onJJov8SvbzLZ7e2Pq36Gum6AubZAEh0TMM7LRUQPDbogq"
+        "69ZyXGD+eeuw9eZGkxiTd0ti3EFT9+OYpNnFRh/KAg==";
 
-    NSString *path = [[NSBundle bundleForClass:[self class]] pathForResource:@"private_key" ofType:@"pem"];
-    NSString *privateKeyString = [NSString stringWithContentsOfFile:path
-                                                           encoding:NSUTF8StringEncoding
-                                                              error:NULL];
-    NSString *expected = @"fQjETWgIMqXEhNyJ52VdsZJIuXX+RHXO4wT5sFtMJBUMTpidksZ8NaQ3Fp1N\nYy2mJEee7Y835IOuFrGpkRroZWnO6s+jvTfWCmaK4DkRKVOB3DbS/o37+PA1\naGVHr7lKh0HHPk6tv7chxUbcdNKC+AWR5/m2DxehhAl23tdyDK2qPr0u2HLv\nzgHNnR4TQafjGOKVAekLdTLu0CUfd8fdzpUEJK5TdUkd6rA5zoORjsLOZXY/\n7cbanCzs2nNii9onJJov8SvbzLZ7e2Pq36Gum6AubZAEh0TMM7LRUQPDbogq\n69ZyXGD+eeuw9eZGkxiTd0ti3EFT9+OYpNnFRh/KAg==\n";
-    
-    NSString *result = [@"password" signWithPrivateKey:[privateKeyString dataUsingEncoding:NSUTF8StringEncoding]
-                                            passphrase:@"727f38dc36b85a68d6bcdd7a7168e0ea3dda10c921117c8b796d7dec9dd1078c"];
-    
-    [[result should] equal:expected];
+        NSString *result = [@"password" signWithPrivateKey:[privateKeyString dataUsingEncoding:NSUTF8StringEncoding]
+                                                passphrase:@"727f38dc36b85a68d6bcdd7a7168e0ea3dda10c921117c8b796d7dec9dd1078c"];
+
+        [[result should] equal:expected];
+    });
 });
 
 SPEC_END
