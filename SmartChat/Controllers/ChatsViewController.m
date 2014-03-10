9e997269de1b468a1da9cc175719c5dfb42e76fa
Tom von Schwerdtner <tom@smartlogic.io>
2014-03-10 15:57:44 -0400

    Displaying chat images.
    This will behave oddly with layered images, but the basic
    functionality is there.

--- SmartChat/Controllers/ChatsViewController.m
+++ SmartChat/Controllers/ChatsViewController.m
@@ -1,11 +1,16 @@
 #import "ChatsViewController.h"
 
 #import <HyperBek/HyperBek.h>
+#import <CommonCrypto/NSData+CommonCrypto.h>
+#import <CommonCrypto/CommonCrypto.h>
 
 #import "HTTPClient.h"
 #import "ChatsView.h"
 #import "ChatCell.h"
 
+#import "ChatViewController.h"
+
+#import "NSData+RSADecrypt.h"
 
 @interface ChatsViewController ()
 
@@ -82,7 +87,38 @@
 - (void)tableView:(UITableView *)tableView accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath
 {
     ChatCell *cell = (ChatCell *)[tableView cellForRowAtIndexPath:indexPath];
-    [cell detailsButtonPressed:nil];
+
+    for (YBHALLink *link in cell.links) {
+        [self.client file:link
+                  success:^(NSURL *filePath, NSString *key, NSString *iv) {
+
+                      // Base64 decode AES Key and AES IV
+                      NSData *decodedKey = [[NSData alloc] initWithBase64EncodedString:key options:0];
+                      NSData *decodedIv = [[NSData alloc] initWithBase64EncodedString:iv options:0];
+
+                      NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+
+                      // RSA Decrypt AES Key and AESIV using stored RSA private kety
+                      NSString *privateKey = [defaults stringForKey:kDefaultsPrivateKey];
+
+                      NSData *decryptedKey = [decodedKey decryptRSA:privateKey password:self.client.passphrase];
+                      NSData *decryptedIv = [decodedIv decryptRSA:privateKey password:self.client.passphrase];
+
+                      // AES Decrypt fileData using decrypted AES Key and decrypted AES IV
+                      NSError *fileError = nil;
+                      NSData *fileData = [NSData dataWithContentsOfURL:filePath options:0 error:&fileError];
+
+                      CCCryptorStatus status = 0;
+                      NSData *decryptedData = [fileData decryptedDataUsingAlgorithm:kCCAlgorithmAES128 key:decryptedKey initializationVector:decryptedIv options:0 error:&status];
+                      UIImage *image = [UIImage imageWithData:decryptedData];
+
+                      ChatViewController *chatViewController = [[ChatViewController alloc] initWithImage:image];
+                      [self.navigationController pushViewController:chatViewController animated:YES];
+
+                  } failure:^(AFHTTPRequestOperation *task, NSError *error) {
+                      NSLog(@"error: %@", error);
+                  }];
+    }
 }
 
 @end
